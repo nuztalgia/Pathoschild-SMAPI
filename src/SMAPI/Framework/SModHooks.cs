8353ea92bf4ad3c1c0965a665e8955703e287af5
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-03-19 17:22:00 -0400

    update for save load changes in SDV 1.6

--- src/SMAPI/Framework/SModHooks.cs
+++ src/SMAPI/Framework/SModHooks.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Threading.Tasks;
+using StardewModdingAPI.Enums;
 using StardewValley;
 
 namespace StardewModdingAPI.Framework
@@ -18,16 +19,21 @@ namespace StardewModdingAPI.Framework
         /// <summary>Writes messages to the console.</summary>
         private readonly IMonitor Monitor;
 
+        /// <summary>A callback to invoke when the load stage changes.</summary>
+        private readonly Action<LoadStage> OnStageChanged;
+
 
         /*********
         ** Public methods
         *********/
         /// <summary>Construct an instance.</summary>
         /// <param name="beforeNewDayAfterFade">A callback to invoke before <see cref="Game1.newDayAfterFade"/> runs.</param>
+        /// <param name="onStageChanged">A callback to invoke when the load stage changes.</param>
         /// <param name="monitor">Writes messages to the console.</param>
-        public SModHooks(Action beforeNewDayAfterFade, IMonitor monitor)
+        public SModHooks(Action beforeNewDayAfterFade, Action<LoadStage> onStageChanged, IMonitor monitor)
         {
             this.BeforeNewDayAfterFade = beforeNewDayAfterFade;
+            this.OnStageChanged = onStageChanged;
             this.Monitor = monitor;
         }
 
@@ -59,6 +65,18 @@ namespace StardewModdingAPI.Framework
             task.RunSynchronously();
             this.Monitor.Log("   task complete.");
             return task;
+        }
+
+        /// <summary>A hook invoked when creating a new save slot, after the game has added the location instances but before it fully initializes them.</summary>
+        public override void CreatedInitialLocations()
+        {
+            this.OnStageChanged(LoadStage.CreatedInitialLocations);
+        }
+
+        /// <summary>A hook invoked when loading a save slot, after the game has added the location instances but before it restores their save data. Not applicable when connecting to a multiplayer host.</summary>
+        public override void SaveAddedLocations()
+        {
+            this.OnStageChanged(LoadStage.SaveAddedLocations);
         }
     }
 }
